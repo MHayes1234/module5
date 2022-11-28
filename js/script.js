f13aefdfc2cd85e498dd6d6cd34ad863634fa549
Matthew <matthewiainhayes@gmail.com>
2022-11-28 20:32:09 +0000

    updated

--- js/script.js
+++ js/script.js
@@ -62,6 +62,7 @@ var switchMenuToActive = function () {
 
 // On page load (before images or CSS)
 document.addEventListener("DOMContentLoaded", function (event) {
+  console.log("Hello")
 
 // TODO: STEP 0: Look over the code from
 // *** start ***
@@ -83,7 +84,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  buildAndShowHomeHTML(), // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -102,8 +103,9 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
-
-      var chosenCategoryShortName = chooseRandomCategory(categories);
+      console.log("got here");
+      var chosenCategoryShortName = chooseRandomCategory();
+      console.log(chosenCategoryShortName);
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
